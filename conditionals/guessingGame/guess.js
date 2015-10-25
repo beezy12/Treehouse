1ec4031b4f17ba751c7fa0680c609890497e83fc
beezy <brian.stumbaugh@gmail.com>
2015-10-25 16:00:08 -0500

    finito for original guessing game

--- conditionals/guessingGame/guess.js
+++ conditionals/guessingGame/guess.js
@@ -1,1 +1,11 @@
-// blah
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
+if (parseInt(guess) === randomNumber) {
+	correctGuess = true;
+}
+if (correctGuess) {
+	document.write("<p>You got it!</p>");
+} else {
+	document.write("<p>errrrrtttt. wrong.</p>");
+}
