b92c2e021a1e0b6c98184b04db2161a95e70032a
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-10-26 21:02:32 -0500

    condy

--- conditionals/guessingGame/guess.js
+++ conditionals/guessingGame/guess.js
@@ -1,11 +1,37 @@
-var correctGuess = false;
+// guess a number between 1 and 6
+// var correctGuess = false;
+// var randomNumber = Math.floor(Math.random() * 6) + 1;
+// var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
+// if (parseInt(guess) === randomNumber) {
+// 	correctGuess = true;
+// }
+// if (correctGuess) {
+// 	document.write("<p>You got it!</p>");
+// } else {
+// 	document.write("<p>errrrrtttt. wrong. The number was " + randomNumber + ".</p>");
+// }
+
+
+
+//same game but tells user if they were too high or too low
+var correctGuess = false;  // set this to false to start, and change it as you go whenever they get it right.
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess) === randomNumber) {
 	correctGuess = true;
+} else if (parseInt(guess) < randomNumber) {
+	var guessMore = prompt("Try again. The number is more than " + guess);
+	if (guessMore === randomNumber) {
+		correctGuess = true;
+	}
+} else if (parseInt(guess) > randomNumber) {
+	var guessLess = prompt("do again. The number is less than " + guess);
+	if (guessLess === randomNumber) {
+		correctGuess = true;
+	}
 }
 if (correctGuess) {
 	document.write("<p>You got it!</p>");
 } else {
-	document.write("<p>errrrrtttt. wrong.</p>");
+	document.write("<p>errrrrtttt. wrong. The number was " + randomNumber + ".</p>");
 }
