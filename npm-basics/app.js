5464ff7ebc92a4723a5052c3390fac6b1f698f84
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-01-23 03:01:14 -0600

    added bcrypt

---
+++ npm-basics/app.js
@@ -0,0 +1,12 @@
+var unsecuredPlainTextPassword = 'password';
+
+// salt is made from a bcrypt method
+// salt = unique string that is used to generate another unique unrecognizable string known as a hash from the
+// original password string.
+// hash =
+var bcrypt = require('bcrypt');
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(unsecuredPlainTextPassword, salt, function(err, hash) {
+        console.log(hash);
+    });
+});
